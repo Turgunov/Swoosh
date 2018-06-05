f90fdb7ff38e50eea0e0ccdf3a7d9afdc609ceb1
Olimjon Turgunov <o.r.turgunov@gmail.com>
2018-06-06 01:20:56 +0200

    Initial Commit

---
+++ swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swoosh
+//
+//  Created by Olimjon Turgunov on 06.06.18.
+//  Copyright © 2018 Olimjon Turgunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
